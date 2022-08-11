4e453cd47c0307a0e8849aa092184f2fc46a56de
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-11 09:45:37 +0300

    Add project files.

---
+++ Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = int.Parse(Console.ReadLine());
+
+            string str = "";
+            for (int i = 1; i <= a; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    str += i + " ";
+                }
+            }
+
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
